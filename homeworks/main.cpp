c02cce20d8439d7b9d2422e1e65b076c95c6b822
oksana <oksanka.kraynova.96@mail.ru>
2018-10-26 22:21:47 +0300

    first task

--- homeworks/main.cpp
+++ homeworks/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cmath>
+class Point {
+private:
+    float x, y;
+public:
+    void get(float a, float b) {
+        x = a, y = b;
+    }
+    float dist() {
+        return sqrt(pow(x,2) + pow(y,2));
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Point p1;
+    p1.get(3, 4);
+    std::cout << p1.dist() << std::endl;
     return 0;
 }
